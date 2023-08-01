f60eefcd0631db0dae9018bc09200f0b571511ea
khainguyen2 <109138008+khainguyen2@users.noreply.github.com>
2023-08-01 11:27:15 +0700

    Update en-vn.js

--- en-vn.js
+++ en-vn.js
@@ -1,39 +1,203 @@
-class AVDictionary {
-  constructor() {
-    // Define your dictionary properties here
-    this.name = 'anhviet';
-    this.displayName = 'en_US-Anh_Viet';
-    this.urlPattern = 'https://your-dictionary.com/search?word={your-word}';
-    this.sourceLang = 'en';
-    this.targetLang = 'vi';
-  }
+/* global api */
+class envn_Cambridge {
+    constructor(options) {
+        this.options = options;
+        this.maxexample = 2;
+        this.word = '';
+    }
 
-  findTerm(word) {
-    return new Promise((resolve, reject) => {
-      // Construct the query URL
-      let queryUrl = this.urlPattern.replace('{your-word}', word);
+    async displayName() {
+        return 'Cambridge EN->VN Dictionary';
+    }
 
-      // Send an AJAX request to the dictionary website
-      let xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          // Parse the response to get the definition
-          let parser = new DOMParser();
-          let doc = parser.parseFromString(xhr.responseText, 'text/html');
-          let definition = doc.querySelector('#definition').innerHTML;
+    setOptions(options) {
+        this.options = options;
+        this.maxexample = options.maxexample;
+    }
 
-          // Resolve the definition to the extension
-          resolve(definition);
-        } else if (xhr.readyState === 4 && xhr.status !== 200) {
-          reject(new Error('Could not fetch definition.'));
+    async findTerm(word) {
+        this.word = word;
+        let promises = [this.findCambridgeEV(word), this.findCambridgeEE(word)];
+        let results = await Promise.all(promises);
+        return [].concat(...results).filter(x => x);
+    }
+
+    async findCambridgeEE(word) {
+        let notes = [];
+        if (!word) return notes; // return empty notes
+
+        function T(node) {
+            if (!node)
+                return '';
+            else
+                return node.innerText.trim();
         }
-      };
-      xhr.open('GET', queryUrl, true);
-      xhr.send();
-    });
-  }
-}
 
-// Create a new instance of the dictionary and add it to the extension
-let avDictionary = new AVDictionary();
-chrome.runtime.sendMessage({type: 'ADD_DICTIONARY', dictionary: avDictionary});
+        let base = 'https://dictionary.cambridge.org/search/english/direct/?q=';
+        let url = base + encodeURIComponent(word);
+        let doc = '';
+        try {
+            let data = await api.fetch(url);
+            let parser = new DOMParser();
+            doc = parser.parseFromString(data, 'text/html');
+        } catch (err) {
+            return [];
+        }
+
+        let entries = doc.querySelectorAll('.pr .entry-body__el') || [];
+        for (const entry of entries) {
+            let definitions = [];
+            let audios = [];
+
+            let expression = T(entry.querySelector('.headword'));
+            let reading_uk = T(entry.querySelector('.uk.dpron-i .pron .ipa'));
+            reading_uk = reading_uk ? `UK[${reading_uk}]` : ''
+            let reading_us = T(entry.querySelector('.us.dpron-i .pron .ipa'));
+            reading_us = reading_us ? `US[${reading_us}]` : ''
+            let reading = [reading_uk, reading_us].join(' ')
+
+            let pos = T(entry.querySelector('.posgram'));
+            pos = pos ? `<span class='pos'>${pos}</span>` : '';
+            audios[0] = entry.querySelector(".uk.dpron-i source");
+            audios[0] = audios[0] ? 'https://dictionary.cambridge.org' + audios[0].getAttribute('src') : '';
+            audios[1] = entry.querySelector(".us.dpron-i source");
+            audios[1] = audios[1] ? 'https://dictionary.cambridge.org' + audios[1].getAttribute('src') : '';
+
+            let sensbodys = entry.querySelectorAll('.sense-body') || [];
+            for (const sensbody of sensbodys) {
+                let sensblocks = sensbody.childNodes || [];
+                for (const sensblock of sensblocks) {
+                    let phrasehead = '';
+                    let defblocks = [];
+                    if (sensblock.classList && sensblock.classList.contains('phrase-block')) {
+                        phrasehead = T(sensblock.querySelector('.phrase-title'));
+                        phrasehead = phrasehead ? `<div class="phrasehead">${phrasehead}</div>` : '';
+                        defblocks = sensblock.querySelectorAll('.def-block') || [];
+                    }
+                    if (sensblock.classList && sensblock.classList.contains('def-block')) {
+                        defblocks = [sensblock];
+                    }
+                    if (defblocks.length <= 0) continue;
+
+                    // make definition segement
+                    for (const defblock of defblocks) {
+                        let eng_tran = T(defblock.querySelector('.ddef_h .def'));
+                        if (!eng_tran) continue;
+                        let definition = '';
+                        eng_tran = `<span class='eng_tran'>${eng_tran.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span>`;
+                        let tran = `<span class='tran'>${eng_tran}</span>`;
+                        definition += phrasehead ? `${phrasehead}${tran}` : `${pos}${tran}`;
+
+                        // make exmaple segement
+                        let examps = defblock.querySelectorAll('.def-body .examp') || [];
+                        if (examps.length > 0 && this.maxexample > 0) {
+                            definition += '<ul class="sents">';
+                            for (const [index, examp] of examps.entries()) {
+                                if (index > this.maxexample - 1) break; // to control only 2 example sentence.
+                                let eng_examp = T(examp.querySelector('.eg'));
+                                definition += `<li class='sent'><span class='eng_sent'>${eng_examp.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span></li>`;
+                            }
+                            definition += '</ul>';
+                        }
+                        definition && definitions.push(definition);
+                    }
+                }
+            }
+            let css = this.renderCSS();
+            notes.push({
+                css,
+                expression,
+                reading,
+                definitions,
+                audios
+            });
+        }
+        return notes;
+    }
+
+    async findCambridgeEV(word) {
+        let notes = [];
+        if (!word) return notes; // return empty notes
+
+        function T(node) {
+            if (!node)
+                return '';
+            else
+                return node.innerText.trim();
+        }
+
+        let base = 'https://dictionary.cambridge.org/search/english-vietnamese/direct/?q=';
+        let url = base + encodeURIComponent(word);
+        let doc = '';
+        try {
+            let data = await api.fetch(url);
+            let parser = new DOMParser();
+            doc = parser.parseFromString(data, 'text/html');
+        } catch (err) {
+            return [];
+        }
+
+        let entries = doc.querySelectorAll('.kdic') || [];
+        for (const entry of entries) {
+            let definitions = [];
+            let audios = [];
+
+            let expression = T(entry.querySelector('.di-title'));
+            let reading = T(entry.querySelector('.ipa'));
+
+            let pos = T(entry.querySelector('.pos'));
+            pos = pos ? `<span class='pos'>${pos}</span>` : '';
+            // definitions.push(`${pos}`)
+            let defblocks = entry.querySelectorAll('.def-block') || [];
+
+            for (const defblock of defblocks) {
+                // definitions.push(`${pos}`)
+                let eng_tran = T(defblock.querySelector('.ddef_h .def'));
+                let chn_tran = T(defblock.querySelector('.def-body .trans'));
+                if (!eng_tran && !chn_tran) continue;
+                let definition = '';
+                eng_tran = `<span class='eng_tran'>${eng_tran.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span>`;
+                chn_tran = `<span class='chn_tran'>${chn_tran}</span>`;
+                let tran = `<span class='tran'>${eng_tran}${chn_tran}</span>`;
+                definition += `${pos}${tran}`;
+                let examps = defblock.querySelectorAll('.def-body .examp') || [];
+                if (examps.length > 0 && this.maxexample > 0) {
+                    definition += '<ul class="sents">';
+                    for (const [index, examp] of examps.entries()) {
+                        if (index > this.maxexample - 1) break; // to control only 2 example sentence.
+                        let eng_examp = T(examp.querySelector('.eg'));
+                        definition += `<li class='sent'><span class='eng_sent'>${eng_examp.replace(RegExp(expression, 'gi'),`<b>${expression}</b>`)}</span></li>`;
+                    }
+                    definition += '</ul>';                    
+                }
+                definition && definitions.push(definition);
+            }
+            let css = this.renderCSS();
+            notes.push({
+                css,
+                expression,
+                reading,
+                definitions,
+                audios
+            });
+        }
+        return notes;
+    }
+
+    renderCSS() {
+        let css = `
+            <style>
+                div.phrasehead{margin: 2px 0;font-weight: bold;}
+                span.star {color: #FFBB00;}
+                span.pos  {text-transform:lowercase; font-size:0.9em; margin-right:5px; padding:2px 4px; color:white; background-color:#0d47a1; border-radius:3px;}
+                span.tran {margin:0; padding:0;}
+                span.eng_tran {margin-right:3px; padding:0;}
+                span.chn_tran {color:#0d47a1;}
+                ul.sents {font-size:0.8em; list-style:square inside; margin:3px 0;padding:5px;background:rgba(13,71,161,0.1); border-radius:5px;}
+                li.sent  {margin:0; padding:0;}
+                span.eng_sent {margin-right:5px;}
+                span.chn_sent {color:#0d47a1;}
+            </style>`;
+        return css;
+    }
+}
